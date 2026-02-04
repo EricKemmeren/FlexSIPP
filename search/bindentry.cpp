5fd77249b685acbc270037909a819234fd103da1
Eric Kemmeren <e.a.kemmeren@student.tudelft.nl>
2026-02-04 12:19:06 +0100

    Pybind setup

---
+++ search/bindentry.cpp
@@ -0,0 +1,57 @@
+#include <pybind11/pybind11.h>
+
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+#include "constants.hpp"
+#include "graph.hpp"
+#include "repeat.hpp"
+#include "structs.hpp"
+
+std::string flexsipp(std::string start, std::string goal, std::string graph_str, intervalTime_t start_time=0, intervalTime_t max_search_time=1000) {
+    Location source_loc(start);
+    Location goal_loc(goal);
+
+    Graph g = read_graph(graph_str);
+
+    bool foundStart = false;
+    bool foundGoal = false;
+    for (GraphNode n: g.node_array) {
+        if (n.state.loc == source_loc) foundStart = true;
+        if (n.state.loc == goal_loc) foundGoal = true;
+    }
+    if (!foundStart) {
+        std::cout << "[ERROR] Start location {" << source_loc.name << "} does not exist in graph\n";
+    }
+    if (!foundGoal) {
+        std::cout << "[ERROR] Goal location {" << goal_loc.name << "} does not exist in graph\n";
+    }
+
+    GraphNode *source = find_earliest(g, source_loc, start_time);
+
+    MetaData m;
+    gamma_t initial_gamma(g.n_agents + 1);
+
+    auto search_start_time = std::chrono::high_resolution_clock::now();
+    auto res = rePEAT::search(source, goal_loc, m, start_time, initial_gamma, max_search_time);
+    auto search_time = std::chrono::high_resolution_clock::now();
+    auto search_duration = std::chrono::duration_cast<std::chrono::milliseconds >(
+            search_time - search_start_time);
+
+    std::stringstream ss;
+
+    std::flush(std::cerr);
+    ss << m << std::endl;
+    ss << res;
+    ss << "Search time: " << search_duration.count() << " milliseconds" << std::endl;
+    std::flush(ss);
+    std::string output = std::move(ss).str();
+    return output;
+}
+
+PYBIND11_MODULE(flexsipp_lib, m) {
+    m.def("flexsipp", &flexsipp);
+}
