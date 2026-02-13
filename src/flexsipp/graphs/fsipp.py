0dbd95ea60be0ae88c1843d96c7fc3e79c7c900a
Eric Kemmeren <erickemmeren@hotmail.com>
2026-02-13 11:08:57 +0100

    Redirect stderr

--- src/flexsipp/graphs/fsipp.py
+++ src/flexsipp/graphs/fsipp.py
@@ -6,17 +6,26 @@ from .graph import Graph
 from ..util.intervals import SafeInterval, FlexibleArrivalTimeFunction
 from ..util.results import Results
 from ..util.types import EdgeType, NodeType
+from ..util.timing import timing
 
 logger = getLogger('__main__.' + __name__)
 
 
 class FSIPP(Generic[EdgeType, NodeType]):
-    def __init__(self, g:Graph[EdgeType, NodeType], heuristic):
+    @timing
+    def __init__(self, g:Graph[EdgeType, NodeType], heuristic, filter_nodes=None):
         g.invert_unsafe_intervals()
         self.atfs: list[FlexibleArrivalTimeFunction] = []
         self.g = g
+        if filter_nodes:
+            self._init_atfs(filter_nodes, heuristic)
+        else:
+            self._init_atfs(g.nodes.values(), heuristic)
 
-        for node in g.nodes.values():
+
+
+    def _init_atfs(self, nodes: list[NodeType], heuristic):
+        for node in nodes:
             def create_atf(from_interval: SafeInterval, edge_interval: SafeInterval, to_interval: SafeInterval, delta):
                 h = heuristic[node.name] if node.name in heuristic else 0
                 flex_atf = FlexibleArrivalTimeFunction(from_interval, edge_interval, to_interval, delta, h)
@@ -39,16 +48,26 @@ class FSIPP(Generic[EdgeType, NodeType]):
                     interval_index_map[interval.index] = last_index
                     last_index += 1
 
-            num_trains = 0
+            unique_trains = set()
             for atf in self.atfs:
                 # TODO: recreate atfs such that from_id and to_id start at 0 (or 1?), also for agents
                 atf = atf.replace_index(interval_index_map)
                 f.write(f"{repr(atf)}\n")
-                num_trains = max(num_trains, atf.train_before.id, atf.train_after.id)
-            f.write(f"num_trains {num_trains}\n")
+                unique_trains.add(atf.train_before.id)
+                unique_trains.add(atf.train_after.id)
+            f.write(f"num_trains {len(unique_trains)}\n")
 
-    def run_search(self, timeout, origin, destination, start_time, file="flexsipp.txt") -> Results:
+    @timing
+    def run_search(self, timeout, origin, destination, start_time, file="flexsipp.txt", err_file="stderr.txt") -> Results:
         self.write(file)
+        print("Running FSIPP: ")
+        print(" ".join(["flexsipp.exe",
+                                   "--start", str(origin),
+                                   "--goal", str(destination),
+                                   "--edgegraph", str(file),
+                                   "--search", "repeat",
+                                   "--startTime", str(start_time)
+                                   ]))
         try:
             proc = subprocess.run(["flexsipp.exe",
                                    "--start", str(origin),
@@ -56,12 +75,14 @@ class FSIPP(Generic[EdgeType, NodeType]):
                                    "--edgegraph", str(file),
                                    "--search", "repeat",
                                    "--startTime", str(start_time)
-                                   ], timeout=timeout, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,
+                                   ], timeout=timeout, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                   encoding='utf-8')
-        except subprocess.TimeoutExpired:
+        except subprocess.TimeoutExpired as to:
             logger.error(f'Timeout for repeat ({timeout}s) expired')
             raise RuntimeError
+        with open(err_file, "w") as f:
+            f.write(proc.stderr)
         if int(proc.returncode) != 0:
             logger.error(f'Search failed for repeat, ec: {proc.returncode}')
             raise RuntimeError
-        return Results(str(proc.stdout))
+        return Results.parse_list_of_outputs(str(proc.stdout))
