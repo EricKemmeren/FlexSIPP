a2fd882450a7f92adb2eb001165471de0f1ea3ca
Eric Kemmeren <54837616+EricKemmeren@users.noreply.github.com>
2026-02-25 10:34:54 +0100

    12 usage of pybind to run flexsipp instead of running an executable (#15)
    * Make string of graph instead of writing to file
    
    * Pybind setup
    
    * Use boost python bind to have search as a as a package
    
    * Use multiple modules
    
    * Remove old entry point
    
    * Update new entry point to use istringstream
    
    * Simplify run_search
    
    * Untrack .idea files
    
    * Update boost python dependency
    
    * Update how the search c++ module is imported
    
    * Update README.md
    
    * Update README.md directories

--- src/flexsipp/graphs/fsipp.py
+++ src/flexsipp/graphs/fsipp.py
@@ -1,14 +1,14 @@
-import subprocess
+import io
 from logging import getLogger
-from typing import Generic
+from typing import Generic, TextIO
 
+from .. import search
 from .graph import Graph
 from ..util.intervals import SafeInterval, FlexibleArrivalTimeFunction
 from ..util.results import Results
 from ..util.types import EdgeType, NodeType
 
 logger = getLogger('__main__.' + __name__)
-
 
 class FSIPP(Generic[EdgeType, NodeType]):
     def __init__(self, g:Graph[EdgeType, NodeType], heuristic):
@@ -24,44 +24,50 @@ class FSIPP(Generic[EdgeType, NodeType]):
                     self.atfs.append(flex_atf)
             [create_atf(*c) for c in node.get_safe_connections()]
 
-    def write(self, file):
-        with open(file, 'wt') as f:
-            f.write(f"vertex count: {str(len([x for node in self.g.nodes.values() for x in node.safe_intervals]))}\n")
-            f.write(f"edge count: {str(len(self.atfs))}\n")
+    def write(self, f:TextIO):
+        f.write(f"vertex count: {str(len([x for node in self.g.nodes.values() for x in node.safe_intervals]))}\n")
+        f.write(f"edge count: {str(len(self.atfs))}\n")
 
-            # Create an index map that maps the safe interval index (in any arbitrary range) to an index starting from 0.
-            interval_index_map: dict[int, int] = {}
-            last_index = 0
+        # Create an index map that maps the safe interval index (in any arbitrary range) to an index starting from 0.
+        interval_index_map: dict[int, int] = {}
+        last_index = 0
 
-            for node in self.g.nodes.values():
-                for interval in node.safe_intervals:
-                    f.write(f"{node.name} {repr(interval)}\n")
-                    interval_index_map[interval.index] = last_index
-                    last_index += 1
+        for node in self.g.nodes.values():
+            for interval in node.safe_intervals:
+                f.write(f"{node.name} {repr(interval)}\n")
+                interval_index_map[interval.index] = last_index
+                last_index += 1
 
-            num_trains = 0
-            for atf in self.atfs:
-                # TODO: recreate atfs such that from_id and to_id start at 0 (or 1?), also for agents
-                atf = atf.replace_index(interval_index_map)
-                f.write(f"{repr(atf)}\n")
-                num_trains = max(num_trains, atf.train_before.id, atf.train_after.id)
-            f.write(f"num_trains {num_trains}\n")
+        num_trains = 0
+        for atf in self.atfs:
+            # TODO: recreate atfs such that from_id and to_id start at 0 (or 1?), also for agents
+            atf = atf.replace_index(interval_index_map)
+            f.write(f"{repr(atf)}\n")
+            num_trains = max(num_trains, atf.train_before.id, atf.train_after.id)
+        f.write(f"num_trains {num_trains}\n")
+
+    # def run_search(self, timeout, origin, destination, start_time, file="flexsipp.txt") -> Results:
+    #     with open(file, 'wt') as f:
+    #         self.write(f)
+    #     try:
+    #         proc = subprocess.run(["flexsipp.exe",
+    #                                "--start", str(origin),
+    #                                "--goal", str(destination),
+    #                                "--edgegraph", str(file),
+    #                                "--search", "repeat",
+    #                                "--startTime", str(start_time)
+    #                                ], timeout=timeout, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,
+    #                               encoding='utf-8')
+    #     except subprocess.TimeoutExpired:
+    #         logger.error(f'Timeout for repeat ({timeout}s) expired')
+    #         raise RuntimeError
+    #     if int(proc.returncode) != 0:
+    #         logger.error(f'Search failed for repeat, ec: {proc.returncode}')
+    #         raise RuntimeError
+    #     return Results(str(proc.stdout))
 
     def run_search(self, timeout, origin, destination, start_time, file="flexsipp.txt") -> Results:
-        self.write(file)
-        try:
-            proc = subprocess.run(["flexsipp.exe",
-                                   "--start", str(origin),
-                                   "--goal", str(destination),
-                                   "--edgegraph", str(file),
-                                   "--search", "repeat",
-                                   "--startTime", str(start_time)
-                                   ], timeout=timeout, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,
-                                  encoding='utf-8')
-        except subprocess.TimeoutExpired:
-            logger.error(f'Timeout for repeat ({timeout}s) expired')
-            raise RuntimeError
-        if int(proc.returncode) != 0:
-            logger.error(f'Search failed for repeat, ec: {proc.returncode}')
-            raise RuntimeError
-        return Results(str(proc.stdout))
+        graph = io.StringIO()
+        self.write(graph)
+        result = search.search(str(origin), str(destination), graph.getvalue(), start_time, timeout)
+        return Results(result)
