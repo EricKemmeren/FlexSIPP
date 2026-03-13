0f55fb4a8ef281d6b548440e55749ae28870ef7a
Eric Kemmeren <erickemmeren@hotmail.com>
2026-03-13 10:45:16 +0100

    Update railway experiment generation

--- experiments/railways/main.py
+++ experiments/railways/main.py
@@ -1,5 +1,7 @@
 import argparse
 
+from flexsipp_railways.train_agents.train_agent_limited_flexibility import \
+    train_agent_limited_flexibility_generator
 from flexsipp_railways.generate import graph_from_file, scenario_from_file
 from flexsipp.graphs.fsipp import FSIPP
 
@@ -14,17 +16,18 @@ parser.add_argument('-e', "--end-time", help="End time of the scenario, if None 
 def run_flexsipp(location_file, scenario_file, delay_agent, scenario_end):
     # TODO the merging of intervals is not from the same agent, so we get an assertion error
     railway_graph = graph_from_file(location_file)
-    scenario = scenario_from_file(scenario_file, railway_graph)
+    scenario = scenario_from_file(scenario_file, railway_graph, agent_cls=train_agent_limited_flexibility_generator(railway_graph.global_end_time))
     scenario.process()
     # TODO do we need to set up flexibility here?
     if delay_agent is None:
         delay_agent = scenario.agents[0]
     else:
         delay_agent = scenario.get_replanning_agent(int(delay_agent))
+    agents = {agent.id: agent for agent in scenario.agents}
     graph = scenario.fsipp(delay_agent)
     heuristic = {node.name: 0 for node in graph.nodes.values()}
-    flexSIPP = FSIPP(graph, heuristic, scenario.agents)
-    result = flexSIPP.run_search(1000, delay_agent.origin.name, delay_agent.destination.name, delay_agent.measures.start_time)
+    flexSIPP = FSIPP(graph, heuristic, agents)
+    result = flexSIPP.run_search(delay_agent.origin.name, delay_agent.destination.name, delay_agent.measures.start_time)
     # TODO readable output
     print(result)
 
