1742cab96b8d76c207bc814efefb68e27292454c
Issa Hanou <i.khanou@tudelft.nl>
2026-03-23 14:41:00 +0100

    update plots

--- experiments/mapf/add_flex_to_plan.py
+++ experiments/mapf/add_flex_to_plan.py
@@ -27,41 +27,42 @@ if __name__ == "__main__":
     results_maeder = {}
     date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-    config_name = "warehouse1"
     config_name = "maze1"
+    config_name = "warehouse1"
 
     location = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "mapf", config_name, configurations[config_name]["location"])
     for scenario in os.listdir(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "mapf", config_name, configurations[config_name]["scenarios"])):
-        if ".txt" in scenario:            
+        if ".txt" in scenario and ("k200" in scenario or "k50" in scenario):
             scenario_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "mapf", config_name, configurations[config_name]["scenarios"], scenario)
             print(scenario)
-            min_gap = 8
+            gaps = [3, 8]
 
-            new_scenario_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "mapf", config_name, "generated", scenario.replace("_paths", f"_paths_{min_gap}"))
-            stats = new_scenario_file.replace(".txt", f".json")
-            with open(scenario_file, "r") as f:
-                paths = [line.split(": ")[1].strip().split("->") for line in f.readlines()]
-                stat_results = {x: [] for x in range(len(paths))}
-                for agent in range(len(paths)):
-                    for other_agent in range(agent+1, len(paths)):
-                        i = 0
-                        while i < len(paths[agent])-1:
-                            loc = paths[agent][i]
-                            j = 0
-                            while j < len(paths[other_agent])-1:
-                                if loc == paths[other_agent][j]:
-                                    if j < i and i-j < min_gap:
-                                        delay_loc = paths[agent][i-1]
-                                        stat_results[agent].append((delay_loc, min_gap-(i-j)))
-                                        for k in range(min_gap-(i-j)):
-                                            paths[agent].insert(i+k, delay_loc)
-                                        i += min_gap-(i-j)
-                                j += 1
-                            i += 1
-                with open(new_scenario_file, "w") as new_file:
-                    for agent, path in enumerate(paths):
-                        new_file.write(f"Agent {agent}: {'->'.join(path)}\n")
-                json.dump(stat_results, open(stats, "w"), indent=4)
+            for min_gap in gaps:
+                new_scenario_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "mapf", config_name, "generated", scenario.replace("_paths", f"_paths_{min_gap}"))
+                stats = new_scenario_file.replace(".txt", f".json")
+                with open(scenario_file, "r") as f:
+                    paths = [line.split(": ")[1].strip().split("->") for line in f.readlines()]
+                    stat_results = {x: [] for x in range(len(paths))}
+                    for agent in range(len(paths)):
+                        for other_agent in range(agent+1, len(paths)):
+                            i = 0
+                            while i < len(paths[agent])-1:
+                                loc = paths[agent][i]
+                                j = 0
+                                while j < len(paths[other_agent])-1:
+                                    if loc == paths[other_agent][j]:
+                                        if j < i and i-j < min_gap:
+                                            delay_loc = paths[agent][i-1]
+                                            stat_results[agent].append((delay_loc, min_gap-(i-j)))
+                                            for k in range(min_gap-(i-j)):
+                                                paths[agent].insert(i+k, delay_loc)
+                                            i += min_gap-(i-j)
+                                    j += 1
+                                i += 1
+                    with open(new_scenario_file, "w") as new_file:
+                        for agent, path in enumerate(paths):
+                            new_file.write(f"Agent {agent}: {'->'.join(path)}\n")
+                    json.dump(stat_results, open(stats, "w"), indent=4)
 
 
 
