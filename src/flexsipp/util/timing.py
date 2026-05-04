5027211afeec1f7322564f16b5afa80d641ed454
Eric Kemmeren <erickemmeren@hotmail.com>
2026-05-04 11:00:03 +0200

    close file in timing decorator

--- src/flexsipp/util/timing.py
+++ src/flexsipp/util/timing.py
@@ -7,15 +7,16 @@ def timing(file_path: Path):
         function_name = str(f).split(" at ", maxsplit=1)[0].split("<function ")[1]
         directory = file_path / "timing"
         directory.mkdir(parents=True, exist_ok=True)
-        file = open(directory / f"{function_name}.csv" , 'w')
-        file.write(f"Seconds\n")
+        with open(directory / f"{function_name}.csv" , 'w') as file:
+            file.write(f"Seconds\n")
         @wraps(f)
         def wrap(*args, **kw):
             ts = time()
             result = f(*args, **kw)
             te = time()
             print(f'Function {str(f).split(" at ", maxsplit=1)[0].split("<function ")[1]} took {te-ts:2.4f} seconds')
-            file.write(f'{te-ts:2.4f}\n')
+            with open(directory / f"{function_name}.csv", 'a') as file:
+                file.write(f'{te-ts:2.4f}\n')
             return result
         return wrap
     return decorator
