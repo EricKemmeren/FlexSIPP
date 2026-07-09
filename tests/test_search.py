7a08133fc86531dbc55e6c0118c8f8588b478021
Issa Hanou <i.khanou@tudelft.nl>
2026-07-09 14:43:59 +0200

    add test on global end time for railways

--- tests/test_search.py
+++ tests/test_search.py
@@ -28,5 +28,48 @@ class TestSearch(unittest.TestCase):
         self.assertEqual(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A'][0][1], 3)
         self.assertEqual(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A'][0][2], 4)
 
+class TestSearchEndTime(unittest.TestCase):
+
+    def test_default_end_time(self):
+        bg = graph_from_file(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "railways", "location_test.json"), global_end_time=None)
+        scenario = scenario_from_file(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "railways", "scenario_test.json"), bg)
+        self.assertEqual(scenario.g.global_end_time, 36)
+        
+        scenario.process()
+        heuristic = {node.name: 0 for node in bg.nodes.values()}
+        self.new_agent = copy(scenario.agents["1"])
+        self.new_agent.id = -1
+        self.flexSIPP = FSIPP(scenario.fsipp(self.new_agent), heuristic, scenario.agents)
+        self.scenario = scenario
+        result = self.flexSIPP.run_search(self.new_agent.origin.name, self.new_agent.destination.name, self.new_agent.measures.start_time)
+        self.assertEqual(len(result.found_routes), 5)
+        self.assertEqual(len(result.unique_routes), 2)
+        self.assertEqual(len(result.unique_path_eatfs), 1)
+        self.assertIn('u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A', result.unique_path_eatfs)
+        self.assertEqual(len(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A']), 3)
+        self.assertEqual(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A'][0][1], 3)
+        self.assertEqual(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A'][0][2], 4)
+
+    def test_custom_end_time(self):
+        end_time = 20
+        bg = graph_from_file(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "railways", "location_test.json"), global_end_time=end_time)
+        scenario = scenario_from_file(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "railways", "scenario_test.json"), bg)
+        self.assertEqual(scenario.g.global_end_time, end_time)
+        
+        scenario.process()
+        heuristic = {node.name: 0 for node in bg.nodes.values()}
+        self.new_agent = copy(scenario.agents["1"])
+        self.new_agent.id = -1
+        self.flexSIPP = FSIPP(scenario.fsipp(self.new_agent), heuristic, scenario.agents)
+        self.scenario = scenario
+        result = self.flexSIPP.run_search(self.new_agent.origin.name, self.new_agent.destination.name, self.new_agent.measures.start_time)
+        self.assertEqual(len(result.found_routes), 3)
+        self.assertEqual(len(result.unique_routes), 1)
+        self.assertEqual(len(result.unique_path_eatfs), 1)
+        self.assertIn('u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A', result.unique_path_eatfs)
+        self.assertEqual(len(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A']), 2)
+        self.assertEqual(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A'][0][1], 3)
+        self.assertEqual(result.unique_path_eatfs['u|A->w|A->s1|A->s2|A->s3|A->s4|A->s5|A->sv|A'][0][2], 4)
+
 if __name__ == '__main__':
     unittest.main()
